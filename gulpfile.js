51c9bb6afc6571abe704c554ee55b2c2bb6099b1
Vlad Savin <Vladislavsavi@gmail.com>
2018-05-14 10:35:07 +0600

    rm idea

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
-var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 // process JS files and return the stream.
